4bcdf1112228c114574270bfcac3cbe217115a10
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-05-28 21:25:12 -0300

    erro de index

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 # Tratamento de erros
 # try, except, else, finally
 
-teste = True
+# Erro de divisão
 
+# teste = True
+
+# try:
+#     a = 10
+#     b = 0
+#     print (a / b)
+# except ZeroDivisionError:
+#     if teste:
+#         raise # Exibe em que linha está o erro
+#     print("Não é possível dividir por zero!")
+
+# erro de index
 try:
-    a = 10
-    b = 0
-    print (a / b)
-except ZeroDivisionError:
-    if teste:
-        raise # Exibe em que linha está o erro
-    print("Não é possível dividir por zero!")
+    lista = [1, 2, 3]
+    print(lista[5])
+except IndexError:
+    print("Índice fora do intervalo")
